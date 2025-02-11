2823a25f881b9c9ce21c6241a60f0eda6fc1464a
aroccon <alessio.roccon@uniud.it>
2025-02-11 15:30:27 +0100

    update

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Tentative porting of MHIT36 to multi GPU using cuDecomp.
 Log of changes/status
 - 06/02/25: MPI init in MHIT36 
 - 07/02/25: Build of cuDecomp ok, present in cuDecomp-main/Build
+- 11/02/25: Up to cuFFT plan, adding comments step by step 
--- multi/poisson_simple.f90
+++ multi/poisson_simple.f90
@@ -22,6 +22,14 @@ program main
   type(cudecompGridDescAutotuneOptions) :: options
   integer :: pdims(2) ! pr x pc pencils
   integer :: gdims(3) ! global grid dimensions
+  type(cudecompPencilInfo) :: piX, piY, piZ  ! size of the pencils in x- y- and z-configuration
+  integer(8) :: nElemX, nElemY, nElemZ, nElemWork
+  ! cuFFT
+  integer :: planX, planY, planZ
+  integer :: batchsize
+  integer :: status
+
+
 
   ! MPI initialization, put in rank the local MPI rank number and ranks total number
   ! Same procedura defined in the cuDecomp documentation
@@ -39,7 +47,7 @@ program main
   ny = 64
   nz = 64
   pr = 2
-  pc = 1
+  pc = 2
   comm_backend = CUDECOMP_TRANSPOSE_COMM_MPI_P2P
 
   CHECK_CUDECOMP_EXIT(cudecompInit(handle, MPI_COMM_WORLD))
@@ -56,12 +64,12 @@ program main
   config%transpose_comm_backend = comm_backend
   config%transpose_axis_contiguous = .true.
 
-  !do some checks 
-  !CHECK_CUDECOMP_EXIT(cudecompGridDescAutotuneOptionsSetDefaults(options))
-  !options%dtype = CUDECOMP_DOUBLE_COMPLEX
-  !if (comm_backend == 0) then
-  !  options%autotune_transpose_backend = .true.
-  !endif
+  !
+  CHECK_CUDECOMP_EXIT(cudecompGridDescAutotuneOptionsSetDefaults(options))
+  options%dtype = CUDECOMP_DOUBLE_COMPLEX
+  if (comm_backend == 0) then
+    options%autotune_transpose_backend = .true.
+  endif
 
   ! initialize cuDecomp with the config file 
   CHECK_CUDECOMP_EXIT(cudecompGridDescCreate(handle, grid_desc, config, options))
@@ -70,6 +78,58 @@ program main
      write(*,"('Running on ', i0, ' x ', i0, ' process grid ...')") config%pdims(1), config%pdims(2)
      write(*,"('Using ', a, ' backend ...')") cudecompTransposeCommBackendToString(config%transpose_comm_backend)
   end if
+
+  ! get pencil info
+  ! This function returns a pencil struct (piX, piY or piZ) that contains the shape, global lower and upper index bounds (lo and hi), 
+  ! size of the pencil, and an order array to indicate the memory layout that will be used (to handle permuted, axis-contiguous layouts).
+  ! Additionally, there is a halo_extents data member that indicates the depth of halos for the pencil, by axis, if provided..
+  ! Side note:  ! cudecompGetPencilInfo(handle, grid_desc, pinfo_x, 1, [1, 1, 1]) <- in this way the x-pencil also have halo elements
+  ! If no halo regions are necessary, a NULL pointer can be provided in place of this array (or omitted)
+  ! Pencil info in x-configuration present in PiX (shape,lo,hi,halo_extents,size)
+  CHECK_CUDECOMP_EXIT(cudecompGetPencilInfo(handle, grid_desc, piX, 1))
+  nElemX = piX%size !<- number of total elments in x-configuratiion (including halo)
+  ! Pencil info in Y-configuration present in PiY
+  CHECK_CUDECOMP_EXIT(cudecompGetPencilInfo(handle, grid_desc, piY, 2))
+  nElemY = piY%size
+  ! Pencil info in Z-configuration present in PiZ
+  CHECK_CUDECOMP_EXIT(cudecompGetPencilInfo(handle, grid_desc, piZ, 3))
+  nElemZ = piZ%size
+
+ ! get workspace size
+  CHECK_CUDECOMP_EXIT(cudecompGetTransposeWorkspaceSize(handle, grid_desc, nElemWork))
+
+ ! show the order 1=x, 2=y, 3=z
+ ! x-pencils are x,y,z
+ ! y-pencils are y,z,x
+ ! z-pencils are z,y,x
+ ! This is to make the setup of cuFFT easier, cuFFT cannot do FFT along inner directions and also for performance (no stride)
+ ! if (rank .eq. 0) then
+ !   write(*,*) "Order in X-pencil is", piX%order(1),piX%order(2),piX%order(3)
+ !   write(*,*) "Order in Y-pencil is", piY%order(1),piY%order(2),piY%order(3)
+ !   write(*,*) "Order in Z-pencil is", piZ%order(1),piZ%order(2),piZ%order(3)
+ ! endif
+
+  ! CUFFT initialization
+  ! Create plans (forward and backward are the same!)
+  batchSize = piX%shape(2)*piX%shape(3) !<- number of FFT (from x-pencil dimension)
+  status = cufftPlan1D(planX, nx, CUFFT_Z2Z, batchSize)
+  if (status /= CUFFT_SUCCESS) write(*,*) rank, ': Error in creating X plan'
+
+  ! it's always 2 and 3 because y-pencil have coordinates y,z,x
+  batchSize = piY%shape(2)*piY%shape(3)
+  status = cufftPlan1D(planY, ny, CUFFT_Z2Z, batchSize)
+  if (status /= CUFFT_SUCCESS) write(*,*) rank, ': Error in creating Y plan'
+
+  ! it's always 2 and 3 because y-pencil have coordinates z,y,x
+  batchSize = piZ%shape(2)*piZ%shape(3)
+  status = cufftPlan1D(planZ, nz, CUFFT_Z2Z, batchSize)
+  if (status /= CUFFT_SUCCESS) write(*,*) rank, ': Error in creating Z plan'
+
+
+
+
+
+
 
 
   call mpi_finalize(ierr)
